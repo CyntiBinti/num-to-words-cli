dd90935f95ab16aef24f23d1bf3b5d0857cd5693
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2023-01-24 22:59:05 +0000

    added more test cases, numbers-to-words function now works up to 9999

--- tests/index.test.js
+++ tests/index.test.js
@@ -14,17 +14,21 @@ describe('Testing the numbersToWords function', () => {
 	});
 
     it('returns the correct words for a "magnitude" number', () => {
+		expect(numbersToWords(100)).toBe("one hundred");
 		expect(numbersToWords(101)).toBe("one hundred and one");
 		expect(numbersToWords(352)).toBe("three hundred and fifty-two");
 		expect(numbersToWords(999)).toBe("nine hundred and ninety-nine");
         expect(numbersToWords(1000)).toBe("one thousand");
 		expect(numbersToWords(9999)).toBe("nine thousand, nine hundred and ninety-nine");
+		expect(numbersToWords(10000)).toBe("ten thousand");
 		expect(numbersToWords(10001)).toBe("ten thousand and one");
         expect(numbersToWords(12055)).toBe("twelve thousand and fifty-five");
         expect(numbersToWords(12300)).toBe("twelve thousand, three hundred");
         expect(numbersToWords(12345)).toBe("twelve thousand, three hundred and forty-five");
 		expect(numbersToWords(99999)).toBe("ninety nine thousand, nine hundred and ninety-nine");
+		expect(numbersToWords(100000)).toBe("one hundred, thousand");
+
 	});
 });
 
-// To-do: create test-cases array export so that dynamically parsed into jest function
+// To-do: create test-cases array export so that dynamically parses into jest function .toBe()
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -3,9 +3,10 @@ const { checkIfValidNumber } = require("../lib/utils");
 describe('Testing the checkIfValidNumber function', () => {
 	it('returns an appropriate error message if user input is invalid', () => {
 		expect(checkIfValidNumber("hello")).toBe("Sorry! I only accept numbers.");
+		expect(checkIfValidNumber(null)).toBe("Sorry! I only accept numbers.");
 		expect(checkIfValidNumber(-23)).toBe("Oops! I only accept positive whole integers between 0 and 100,000.");
-        expect(checkIfValidNumber(null)).toBe("Sorry! I only accept numbers.");
         expect(checkIfValidNumber(100005)).toBe("Oops! I only accept positive whole integers between 0 and 100,000.");
+		expect(checkIfValidNumber(12.34)).toBe("Oops! I only accept positive whole integers between 0 and 100,000.");
 	});
 
     it('returns true if user input is a valid number between 0 and 100,000', () => {
